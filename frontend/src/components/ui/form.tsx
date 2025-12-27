8c927a2b3a6a2c2e33bb69e7e1270c637916e51f
ThousandGolder <kumaworku22@gmail.com>
2025-12-27 22:25:03 +0300

    adding files the componet

---
+++ frontend/src/components/ui/form.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface FormProps extends React.FormHTMLAttributes<HTMLFormElement> {
+  children: React.ReactNode;
+}
+
+const Form = React.forwardRef<HTMLFormElement, FormProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <form ref={ref} className={cn("space-y-6", className)} {...props}>
+        {children}
+      </form>
+    );
+  }
+);
+Form.displayName = "Form";
+
+const FormItem = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("space-y-2", className)} {...props} />;
+});
+FormItem.displayName = "FormItem";
+
+const FormLabel = React.forwardRef<
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <label
+      ref={ref}
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className
+      )}
+      {...props}
+    />
+  );
+});
+FormLabel.displayName = "FormLabel";
+
+const FormControl = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("relative", className)} {...props} />;
+});
+FormControl.displayName = "FormControl";
+
+const FormDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      className={cn("text-sm text-muted-foreground", className)}
+      {...props}
+    />
+  );
+});
+FormDescription.displayName = "FormDescription";
+
+const FormMessage = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      className={cn("text-sm font-medium text-destructive", className)}
+      {...props}
+    >
+      {children}
+    </p>
+  );
+});
+FormMessage.displayName = "FormMessage";
+
+interface FormFieldProps {
+  children: React.ReactNode;
+}
+
+const FormField = ({ children }: FormFieldProps) => {
+  return <>{children}</>;
+};
+
+export {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+};
